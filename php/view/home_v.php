591c8a5970660d114a160a547e11f341a42d5f2b
Bill Rocha <ahcordesign@gmail.com>
2013-12-25 01:36:40 -0200

    version 0.0.2

--- php/view/home_v.php
+++ php/view/home_v.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $db = $mega->getAll('D1, D2, D3, D4, D5, D6, ACERTO');
 
 $concursos = count($db);
@@ -11,53 +11,56 @@ $numFinal = 60;
 
 //tamanho dos quadrados
 $retW = 15;
-
 ?>
 <div class="grafico">
-	<h2>Mapeamento de Resultados</h2>
-        <p>As dezenas em vermelho são de concursos com ACERTO</p>
-	<svg height="<?php echo 40 + ($concursos * $retW);?>" id="calendar-graph">
-		<g transform="translate(40, 20)">
+    <h2>Mapeamento de Resultados</h2>
+    <p>As dezenas em vermelho são de concursos com ACERTO</p>
+    <svg height="<?php echo 40 + ($concursos * $retW); ?>" id="calendar-graph">
+        <g transform="translate(40, 20)">
 
-			<?php
+        <?php
+        //Texto Horizontal com os números
+        $dx = 4;
+        $dy = -5;
+        for ($i = $numInicial; $i <= $numFinal; $i ++) {
+            echo '<text text-anchor="middle" class="wday" dx="' . $dx . '" dy="' . $dy . '" >' . $i . '</text>';
+            $dx += $retW;
+        }
 
-			//Texto Horizontal com os números
-			$dx = 4;
-			$dy = -5;
-			for($i = $numInicial; $i <= $numFinal ; $i ++){
-				echo '
-				<text text-anchor="middle" class="wday" dx="'.$dx.'" dy="'.$dy.'" >'.$i.'</text>';
-				$dx += $retW;
-			}
+        //Texto vertical com os concursos
+        $dy = 9;
+        for ($i = $concInicial; $i <= $concFinal; $i ++) {
+            echo '<text text-anchor="end" text-align="right" class="vday" dx="-20" dy="' . $dy . '" >' . $i . '</text>';
+            $dy += $retW;
+        }
 
-			//Texto vertical com os concursos
-			$dy = 9;
-			for($i = $concInicial; $i <= $concFinal ; $i ++){
-				echo '
-				<text text-anchor="end" text-align="right" class="vday" dx="-20" dy="'.$dy.'" >'.$i.'</text>';
-				$dy += $retW;
-			}
+        //comcursos
+        $t1 = 0;
 
-			//comcursos
-			$t1 = 0;
+        //cada número						
+        for ($n = $numInicial; $n <= $numFinal; $n ++) {
+            echo '<g transform="translate(' . $t1 . ', 0)">';
+            $t1 += $retW;
 
-			//cada número						
-			for($n = $numInicial; $n <= $numFinal ; $n ++){
-				echo '<g transform="translate('.$t1.', 0)">';
-				$t1 += $retW;
+            $y = 0;
+            //cada concurso
+            for ($i = $concInicial; $i <= $concFinal; $i ++) {
+                $cor = ($db[($i - 1)][6] >= 1) ? 'F00' : 'BBB';
+                if ($db[($i - 1)][0] != $n &&
+                    $db[($i - 1)][1] != $n &&
+                    $db[($i - 1)][2] != $n &&
+                    $db[($i - 1)][3] != $n &&
+                    $db[($i - 1)][4] != $n &&
+                    $db[($i - 1)][5] != $n) $cor = 'EEE';
 
-				$y = 0;
-				for($i = $concInicial; $i <= $concFinal; $i ++){
-                                        $cor = ($db[($i - 1)]['ACERTO'] >= 1) ? 'F00':'BBB';
-					echo '<rect class="day" width="'.($retW - 2).'" height="'.($retW - 2).'" y="'.$y.'" style="fill: #'.
-					((in_array($n, $db[($i - 1)])) ? $cor : 'DDD').';"></rect>';
-					$y += $retW;
-				}
+                echo '<rect class="day" width="' . ($retW - 2) . '" height="' . ($retW - 2) . '" y="' . $y . '" style="fill: #' . $cor . ';"></rect>';
+                $y += $retW;
+            }
 
-				//fechando
-				echo '</g>';
-			}
-			?>
-		</g>
-	</svg>
+            //fechando cada número
+            echo '</g>';
+        }
+        ?>
+        </g>
+    </svg>
 </div>
