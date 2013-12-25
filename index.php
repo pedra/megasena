328ef5604d183c76392e5763f0e4bfa6616381d4
Bill Rocha <ahcordesign@gmail.com>
2013-12-24 22:43:47 -0200

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,11 @@
 <?php 
-/*include 'php/lib/neos/core.php';
-Main::run();*/
-
-
 include 'php/lib/megasena/mega.php';
+
 $mega = new Mega();
 $mega->setTmpDir(__DIR__.'/php/lib/megasena/temp');
 $mega->setDbType('mysql');
-$mega->setDataBase('megasena');
-$mega->setPassword('xmegasena123');
 
+//Create DBase
 //$mega->createDb(); exit('criando DB - fim');
 
 //update
@@ -17,25 +13,6 @@ $mega->setPassword('xmegasena123');
 
 if($mega->getError() != null) exit('<p style="color:#F00">Error: '.$mega->getError().'</p>');
 
-//exit($mega->getprintall());
-
-
-//echo '<pre>'.print_r($mega->result(), true).'</pre>';
-
-
-
-define('TPATH', __DIR__.'/php/lib/megasena/temp/');
-
 include 'php/view/head.html';
-//include 'php/view/home.php';
 echo $mega->getCont();
 include 'php/view/footer.html';
-
-
-
-// ===================================
-function p($v, $x = false){
-	$v = '<pre>'.print_r($v, true).'</pre>';
-	if($x) exit($v);
-	else echo $v;
-} 
